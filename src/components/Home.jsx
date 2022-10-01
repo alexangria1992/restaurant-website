bd2b8391fb29e65a5dc7ab145388dfd741a80c2f
alexangria1992 <alex040892@gmail.com>
2022-10-01 18:48:35 +1300

    Navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles/Home.module.scss";
+import Navbar from "../components/Navbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
